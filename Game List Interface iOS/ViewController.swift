4cd180ea2187c671db288cbe36248b44f58c3c18
Bhavesh Goswami <goswamjbhavesh@gmail.com>
2025-12-01 12:07:16 -0600

    Initial Commit

---
+++ Game List Interface iOS/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Game List Interface iOS
+//
+//  Created by Bhavesh on 12/1/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Game List Interface iOSTests/Game_List_Interface_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  Game_List_Interface_iOSTests.swift
+//  Game List Interface iOSTests
+//
+//  Created by Bhavesh on 12/1/25.
+//
+
+import Testing
+@testable import Game_List_Interface_iOS
+
+struct Game_List_Interface_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
